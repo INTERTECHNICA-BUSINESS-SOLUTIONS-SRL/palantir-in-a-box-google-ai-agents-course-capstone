0ee99b2ad7c5617cde11b6156aa6bd7a50105c7e
Marin Iuga <marin.iuga@intertechnica.com>
2025-11-29 17:47:36 +0200

    Minor improvements.

--- setup.py
+++ setup.py
@@ -4,8 +4,25 @@ sys.dont_write_bytecode = True
 import os
 import logging
 
+import asyncio
+import platform
+
 from dotenv import load_dotenv
 
+def suppress_useless_logging():
+    """
+    Suppresses some useless logging.
+    """
+    logging.getLogger("xhtml2pdf").setLevel(logging.ERROR)
+        
+def potential_fix_for_event_loops():
+    """
+    Sets the event loop policy to WindowsSelectorEventLoopPolicy on Windows.
+    This prevents 'RuntimeError: Event loop is closed' in most of the cases.
+    """
+    if platform.system() == 'Windows':
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+        
 def initialize_environment_variables():
     """
     Initializes environment variables for the application.
@@ -22,3 +39,10 @@ def initialize_environment_variables():
 
 # initialize environment variables
 initialize_environment_variables()
+
+# prevent logging noise
+suppress_useless_logging()
+
+# fix potential errors with event loops
+potential_fix_for_event_loops()
+
