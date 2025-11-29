fac0f4bde0beaacf00e8c61871f3be8dacdaad0b
Marin Iuga <marin.iuga@intertechnica.com>
2025-11-29 10:48:28 +0200

    Better enforcement of tool calling when performing content retrieval.

--- agents/content_retrieval_agent_factory.py
+++ agents/content_retrieval_agent_factory.py
@@ -16,18 +16,17 @@ class ContentRetrievalAgentFactory(BaseGeminiLLMAgentFactory):
     def _get_instruction(self) -> str:
         instruction = f"""
             <role>
-                You are an expert agent specialized in content retrieval operations using URL addresses by using the '{retrieve_content_from_url.__name__}' tool.
+                You are an expert agent specialized in content retrieval operations using URL addresses by using the '{retrieve_content_from_url.__name__}'. 
+                THis tool which must ALWAYS be called in the process of content retrieval. Never provide your own information.
                 The content retrieval procedure is described in the task description and you MUST call the tool provided in the task_description.
             </role>
             <task_description>
                 You will receive a web URL from which you will have to retrieve the content using the following rules:
                 
-                1. Check that the URL has been provided;
-                2. If the URL has been provided, you MUST call the '{retrieve_content_from_url.__name__}' tool to retrieve the content from the URL.
+                1. You MUST ALWAYS call the '{retrieve_content_from_url.__name__}' tool to retrieve the content from the URL.
                 Respond with the content returned by the '{retrieve_content_from_url.__name__}' tool without any changes or modifications.
-                3. If no URL has been provided, respond with '[NO URL PROVIDED]'.
+                2. You MUST ALWAYS reply with the content exactly as returned by the tool. No modifications to it!
 
-                If no content could be retrieved, respond with '[NO CONTENT]'.
                 IMPORTANT: Never return an empty answer.
             </task_description>
             <format>
